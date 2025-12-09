456f186c7ff45b88d1f1ecc2c13fb2287ef6c745
Senorita-Marg14 <margaretfholley@gmail.com>
2025-12-09 11:59:13 -0600

    added sorting task by due date

--- react-frontend/src/components/TaskList.js
+++ react-frontend/src/components/TaskList.js
@@ -5,10 +5,11 @@ export default function TaskList({ tasks, onDelete, onToggleComplete, userId }) 
   if (!tasks.length) {
     return <div className="card"><p>No tasks found.</p></div>;
   }
+  const sortTasks = tasks ? [...tasks].sort((a,b) => {if (!a.dueDate) return 1; if(!b.dueDate) return -1; return new Date(a.dueDate) - new Date(b.dueDate);}) : [];
 
   return (
     <div>
-      {tasks.map((t) => (
+      {sortTasks.map((t) => (
         <TaskCard 
           key={t._id} 
           task={t} 
